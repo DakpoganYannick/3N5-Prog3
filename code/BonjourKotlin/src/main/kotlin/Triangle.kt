42b2b652a04af8d5ebe1b492904bc5236bcb6fdb
DakpoganYannick <6238437@cegepmontpetit.ca>
2024-08-22 12:39:58 -0400

    Exercice Triangle

---
+++ code/BonjourKotlin/src/main/kotlin/Triangle.kt
@@ -0,0 +1,19 @@
+package org.dakpogan
+
+fun main() {
+
+
+    val nbRangees = 4
+
+    for(i in 1..nbRangees)
+    {
+        // .repeat repète le caractère "i" fois
+        val nbCaractere = "*".repeat(i)
+
+        println(nbCaractere)
+
+
+    }
+
+
+}
